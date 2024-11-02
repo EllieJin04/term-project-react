1af6bf5b7e289419116db887c52d327c9416f287
EllieJin04 <elliejin2004@gmail.com>
2024-11-02 14:27:18 -0400

    Add some routes in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React from 'react';
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Search from "./Search";
+import Review from "./Review";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/search" element={<Search />}/>
+          <Route path="/review" element={<Review />}/>
         </Routes>
       </div>
     </Router>
