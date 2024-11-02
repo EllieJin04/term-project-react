62686b1f9509b2644c98c157941fafa0fd43787e
hj23198 <hj23198@students.mcpasd.k12.wi.us>
2024-11-02 15:50:52 -0400

    register page

--- src/Register.js
+++ src/Register.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import {Link} from "react-router-dom"
 
 const Register = () => {
+    //TODO replace icon with svg
     return (  
-        <div className="main"></div>
+        <div className="login">
+            <div class="flexcenter padtop">
+                <div class="flexcol" >
+
+                    <div class="flexcenter">
+                        <img src='favicon.ico' class="roselogo"></img>
+                    </div>
+
+                    <div class="flexcenter">
+                        <h1>Register</h1>
+                    </div>
+
+                    <div class="flexcenter">
+                        <input type='text' placeholder='Username'></input>
+                    </div>
+
+                    <div class="flexcenter">
+                        <input type='text' placeholder='Password'></input>
+                    </div>
+
+                    <div class="flexcenter">
+                        <button>LOGIN</button>
+                    </div>
+
+                    <div class="flexcenter">
+                        <p class="footmsg">Already have an account?&#9;<a>SIGN UP</a></p>
+                    </div>
+
+
+                    <div class="flexcenter">
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     );
 }
- 
+
 export default Register;
