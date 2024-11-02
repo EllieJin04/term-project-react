93df0655ba7f7ed70ba4e80fbdaba65d2735093c
EllieJin04 <elliejin2004@gmail.com>
2024-11-02 14:48:41 -0400

    Home page and css

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,10 @@ const Home = () => {
     return (  
         <div className="main">
             <div className="favorite-dish">
-                
+                <div className="title">Favorite Dish:</div>
             </div>
             <div className="todays-dish">
-
+                <div className="title">Today's Dish:</div>
             </div>
         </div>
     );
