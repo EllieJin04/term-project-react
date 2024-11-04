688e79e3f5682d6be86969ba00cbb1dbfc1bcf49
EllieJin04 <elliejin2004@gmail.com>
2024-11-04 08:24:00 -0500

    body part of search page

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,10 @@ import {Link} from "react-router-dom"
 const Search = () => {
     return (  
         <div className="main">
-
+            <form action="/review">
+                <input type="text" className="search-input"/>
+                <button type="submit" className="search-button">Search</button>
+            </form>
         </div>
     );
 }
